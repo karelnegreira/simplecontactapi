f8bee8e0339f72b1b03c8622dbc4eaef1f173bfc
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-03-08 09:20:54 +1200

    Lombok annotations added to Contact.java

--- src/main/java/com/example/apicontactapp/domain/Contact.java
+++ src/main/java/com/example/apicontactapp/domain/Contact.java
@@ -1,5 +1,20 @@
 package com.example.apicontactapp.domain;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="CONTACT")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class Contact {
     private String id;
     private String name;
@@ -9,4 +24,20 @@ public class Contact {
     private String address;
     private String status;
     private String photoUrl;
+
+    /*
+    public Contact() {
+        //Demanded by JPA
+    }
+
+    public Contact(String name, String email, String title, String phone, String address, String status, String photoUrl) {
+        this.name = name;
+        this.email = email;
+        this.title = title;
+        this.phone = phone;
+        this.address = address;
+        this.status = status;
+        this.photoUrl = photoUrl;
+    }
+    */
 }
