280ccd5bc7b8ce6a7e6c407f79dfdc8c808072a6
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-03-08 09:36:53 +1200

    ContactRepository declared as JpaRepository

--- src/main/java/com/example/apicontactapp/domain/Contact.java
+++ src/main/java/com/example/apicontactapp/domain/Contact.java
@@ -1,21 +1,27 @@
 package com.example.apicontactapp.domain;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
 
 @Entity
-@Table(name="CONTACT")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
+@Table(name="contacts")
 public class Contact {
+    @Id
+    @UuidGenerator
+    @Column(name = "id", unique = true, updatable = false)
     private String id;
     private String name;
     private String email;
