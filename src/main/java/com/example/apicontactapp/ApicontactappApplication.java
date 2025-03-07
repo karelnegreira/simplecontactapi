c83fbef0a2e1497d6337ffda924439644f3ff7be
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-03-08 09:12:49 +1200

    First commit with Contact class created

---
+++ src/main/java/com/example/apicontactapp/ApicontactappApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicontactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicontactappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicontactappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apicontactapp/ApicontactappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apicontactapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicontactappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
