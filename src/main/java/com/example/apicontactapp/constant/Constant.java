e1fcc7c85eab55905368576b26e9c10bd4904512
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-03-20 00:24:49 +1200

    CorsConfig file in backend app setted up and ready

--- src/main/java/com/example/apicontactapp/constant/Constant.java
+++ src/main/java/com/example/apicontactapp/constant/Constant.java
@@ -2,4 +2,5 @@ package com.example.apicontactapp.constant;
 
 public class Constant {
     public static final String PHOTO_DIRECTORY = System.getProperty("user.home") + "/Downloads/uploads/";
+    public static final String X_REQUESTED_WITH = "X-Requested-With" ;
 }
